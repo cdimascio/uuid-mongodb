6b51b85bfc71a94342e4dbe8521857d36cc1d41f
Carmine DiMascio <dimascio@us.ibm.com>
2018-08-08 21:55:31 -0400

    implement tests

--- test/uuid-mongodb_spec.js
+++ test/uuid-mongodb_spec.js
@@ -4,18 +4,26 @@ const uuidv4 = require('uuid/v4');
 const { Binary } = require('mongodb');
 const MUUID = require('../lib');
 
-describe('MUUID', function() {
+const hasHexUpperCase = s => /[A-F]/.exec(s);
+
+describe('MUUID (accept and generate uuids according to spec - (see https://www.itu.int/rec/T-REC-X.667-201210-I/en)', function() {
   describe('v1()', () => {
     it('should generate a valid v1 uuid', function() {
-      const mUUID1 = MUUID.v1();
-      assert.equal(validate(mUUID1.toString()), true);
+      const mUUID = MUUID.v1();
+      assert.equal(validate(mUUID.toString()), true);
+      // ensure generated uuids are always lowercase
+      // as per spec - (see https://www.itu.int/rec/T-REC-X.667-201210-I/en)
+      assert.equal(hasHexUpperCase(mUUID.toString(), false));
     });
   });
 
   describe('v4()', () => {
     it('should generate a validate v4 uuid', function() {
-      const mUUID1 = MUUID.v4();
-      assert.equal(validate(mUUID1.toString()), true);
+      const mUUID = MUUID.v4();
+      assert.equal(validate(mUUID.toString()), true);
+      // ensure generated uuids are always lowercase
+      // as per spec - (see https://www.itu.int/rec/T-REC-X.667-201210-I/en)
+      assert.equal(hasHexUpperCase(mUUID.toString(), false));
     });
   });
 
@@ -25,8 +33,20 @@ describe('MUUID', function() {
       assert.equal(validate(mUUID.toString()), true);
     });
 
+    it('should accept uuid\'s with capital letters, but return with lowercase', () => {
+      const mUUID = MUUID.from(uuidv4().toUpperCase());
+      assert.equal(hasHexUpperCase(mUUID.toString(), false));
+      assert.equal(validate(mUUID.toString()), true);
+    });
+
+    it('should convert uuid from string', () => {
+      const mUUID = MUUID.from(uuidv4());
+      assert.equal(validate(mUUID.toString()), true);
+    });
+
     it('should throw when converting an invalid uuid', () => {
       assert.throws(() => MUUID.from('invalid-uuid'), 'Invalid uuid');
+      assert.throws(() => MUUID.from('802ac0f09b7311e89bb69baebe1aa0bf'), 'Invalid uuid');
     });
 
     it('should convert uuid from mongo BSON binary', () => {
