59c9a856bd1b926cc52ce1a2758923ad84b81a5f
Carmine DiMascio <cdimascio@tripadvisor.com>
2019-11-26 00:20:49 -0500

    update examples

--- example/ex3-mongoose.js
+++ example/ex3-mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const MUUID = require('../lib');
 
+const log = (...a) => console.log(...a);
 // Setup and connect
 mongoose.connect('mongodb://localhost/my_mongoose', {
   useNewUrlParser: true,
@@ -21,24 +22,34 @@ const kittySchema = new mongoose.Schema({
   title: String,
 });
 
+// no need for auto getter for _id will add a virtual later
+kittySchema.set('id', false);
+
+// virtual getter for custom _id
+kittySchema
+  .virtual('id')
+  .get(function() {
+    return MUUID.from(this._id).toString();
+  })
+  .set(function(val) {
+    this._id = MUUID.from(val);
+  });
+
 const Kitten = mongoose.model('Kitten', kittySchema);
 
-// 2. Create new kitten with UUID _id
+// 2. Create new kitten
 var silence = new Kitten({
-  // _id: MUUID.v1(),
   title: 'Silence',
 });
 
 // 3. Save the new kitten to the database
 silence
   .save()
-  // 4. Fetch the new kitten from the database
   .then(kitten => {
-    console.log('inserted kitten with id', MUUID.from(kitten._id).toString());
-    return Kitten.findOne({ _id: kitten._id });
+    log('inserted kitten with id', kitten.id);
+    return kitten._id;
   })
-  .then(kitten =>
-    console.log('found kitten with id', MUUID.from(kitten._id).toString())
-  )
-  .catch(e => console.error(e))
+  .then(_id => Kitten.findOne({ _id }))
+  .then(kitten => log('found kitten with id', kitten.id))
+  .catch(e => log(e))
   .finally(() => db.close());
