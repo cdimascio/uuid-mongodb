79fd8c0a5f60824e5ac0d718778ca6a6954d36f8
Erich Kuba <erich.kuba@api.games>
2020-12-17 09:25:56 +1300

    Implemented the specific types used in the mode function so as not to break downstream typescript projects that have the compiler's "noImplicitAny" flag set to true.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,14 @@
 import { Binary } from "mongodb";
 
 export type MUUID = Binary;
+export type MODE = {
+  v1: () => MUUID,
+  v4: () => MUUID,
+  from: (uuid: string | Binary) => MUUID,
+  mode: MODE
+}
 
 export const v1: () => MUUID;
 export const v4: () => MUUID;
 export const from: (uuid: string | Binary) => MUUID;
-export const mode: (mode: 'canonical'| 'relaxed') => { v1, v4, from, mode};
+export const mode: (mode: 'canonical'| 'relaxed') => MODE;
