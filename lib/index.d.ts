ee26ce5dbb7a363f703c4b9968319a0a38ad16cf
Carmine DiMascio <cdimasci@amazon.com>
2020-12-25 11:53:40 -0500

    feat: improved type declarations

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,14 +1,14 @@
 import { Binary } from "mongodb";
 
 export type MUUID = Binary;
-export type MODE = {
+export type Mode = {
   v1: () => MUUID,
   v4: () => MUUID,
   from: (uuid: string | Binary) => MUUID,
-  mode: MODE
+  mode: Mode
 }
 
 export const v1: () => MUUID;
 export const v4: () => MUUID;
 export const from: (uuid: string | Binary) => MUUID;
-export const mode: (mode: 'canonical'| 'relaxed') => MODE;
+export const mode: (mode: 'canonical'| 'relaxed') => Mode;
